2ae8f11f743d9e3be08c75c8f4439497a385e912
Merijn van Ginkel <merijn@notfound.nl>
2024-03-12 16:14:28 +0100

    feat: add clicks and opens to listboss results

--- src/Http/Controllers/ListBossController.php
+++ src/Http/Controllers/ListBossController.php
@@ -45,16 +45,30 @@ class ListBossController extends Controller
 
         $table->addHeader(new LayoutTableHeader('E-mailadres', 'email'));
         $table->addHeader(new LayoutTableHeader('Ontvangen', 'received'));
-        $table->addHeader(new LayoutTableHeader('Geklikt', 'clicks'));
-        $table->addHeader(new LayoutTableHeader('Geopend', 'opens'));
+        $table->addHeader((new LayoutTableHeader('Status', 'status'))->sortable());
+        $table->addHeader((new LayoutTableHeader('Geopend', 'opens'))->sortable());
+        $table->addHeader((new LayoutTableHeader('Geklikt', 'clicks'))->sortable());
 
         $rowId = 1;
-        foreach ($job->result()->results as $result) {
+
+        $validated = $request->validate([
+            'sort' => 'string',
+            'asc' => 'string'
+        ]);
+
+        $jobResults = $job->result()->results;
+
+        if (!empty($validated['sort']) && !empty($validated['asc'])){
+            $jobResults = collect($jobResults)->sortBy($validated['sort'], 0, $validated['asc'] == 'true')->reverse()->toArray();
+        }
+
+        foreach ($jobResults as $result) {
             $row = new LayoutTableRow($rowId++, '/app/listboss/'.$job->id().'/'.$result->id);
             $row->addColumn(new LayoutTableColumn($result->email));
             $row->addColumn(new LayoutTableColumn(\Sb::formatDate($result->delivered_at)));
-            $row->addColumn(new LayoutTableColumn($result->clicks));
+            $row->addColumn(new LayoutTableColumn($result->status ?? '-'));
             $row->addColumn(new LayoutTableColumn($result->opens));
+            $row->addColumn(new LayoutTableColumn($result->clicks));
             $table->addRow($row);
         }
 
@@ -65,9 +79,12 @@ class ListBossController extends Controller
 
     private function statusText(object $statusInfo): LayoutText
     {
-        $text = '<p>Aantal ontvangers: '.($statusInfo->recipients ?? '-').
-        '<p>Voortgang: <strong>'.($statusInfo->progress ?? '0').'%</strong>'.
-        '<p>Fouten: '.($statusInfo->errors ?? '0');
+        $text =
+            '<p>Aantal ontvangers: '.($statusInfo->recipients ?? '-')."</p>".
+            '<p>Aantal keer geopend in mailprogramma: '.($statusInfo->opens ?? '-')."</p>".
+            '<p>Aantal keer geklikt op links: '.($statusInfo->clicks ?? '-')."</p>".
+            '<p>Voortgang: <strong>'.($statusInfo->progress ?? '0').'%</strong>'.
+            '<p>Fouten: '.($statusInfo->errors ?? '0');
 
         return new LayoutText($text);
     }
